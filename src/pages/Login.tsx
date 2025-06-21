13ce9b5291515737f889d3baa3576c80ff8f27cb
designhexa <157108416+designhexa@users.noreply.github.com>
2025-06-21 18:46:11 +0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -54,7 +54,7 @@ const Login = () => {
     <div 
       className="min-h-screen flex items-center justify-center relative"
       style={{
-        backgroundImage: `url('https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80')`,
+        backgroundImage: `url('https://cdn.pixabay.com/photo/2017/10/22/14/36/technoid-2877987_1280.jpg')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat'
